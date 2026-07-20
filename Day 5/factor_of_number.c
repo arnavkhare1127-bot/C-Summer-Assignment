511aa61be0a347f1e839340a921c02bcb7ba2531
Arnav Khare <your-real-github-email@example.com>
2026-07-21 01:44:17 +0530

    commit

---
+++ Day 5/factor_of_number.c
@@ -0,0 +1,23 @@
+// Write a program to Print factors of a number.
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the number");
+    scanf("%d", &n);
+    if (n <= 0)
+    {
+        printf("Enter a number greater than zero");
+        return 1;
+    }
+    printf("The factors of %d are ", n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
