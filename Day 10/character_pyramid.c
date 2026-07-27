9c48ad9f714449b3d262f19037b272fc93926b25
Arnav Khare <arnavkhare11.27@gmail.com>
2026-07-28 02:18:34 +0530

    commit

---
+++ Day 10/character_pyramid.c
@@ -0,0 +1,32 @@
+/*Write a program to Print character pyramid.
+    A
+   ABA
+  ABCBA
+ ABCDCBA
+ABCDEDCBA
+*/
+#include<stdio.h>
+int main()
+{
+    int rows,i,j,space;
+    printf("Enter number of rows");
+    scanf("%d",&rows);
+
+    for(i=1;i<=rows;i++)
+    {
+        for(space=1;space<=(rows-i);space++)
+        {
+        printf(" ");
+        }
+    
+    for(j=1;j<=i;j++)
+    {
+        printf("%c",'A'+j-1);
+    }
+    for(j=i-1;j>=1;j--)
+    {
+        printf("%c",'A'+j-1);
+    }
+    printf("\n");
+    }
+}
