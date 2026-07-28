3b775e9f555ca6c0d5fe9164618244d5b404bfdb
Arnav Khare <arnavkhare11.27@gmail.com>
2026-07-29 01:27:14 +0530

    commit

---
+++ Day 11/find_factorial.c
@@ -0,0 +1,29 @@
+// Write a program to Write function to find factorial.
+#include <stdio.h>
+int findfactorial(int n);
+int main()
+{
+    int num, fact;
+    printf("Enter the number");
+    scanf("%d", &num);
+
+    if (num < 0)
+    {
+        printf("Factorial of a negative number does not exist");
+    }
+    else
+    {
+        fact = findfactorial(num);
+        printf("Factorial of %d =%d\n", num, fact);
+    }
+    return 0;
+}
+int findfactorial(int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
